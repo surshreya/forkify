32df6489b2f485c25d01942a00070d994331d4d9
surshreya <surshreya.0727@gmail.com>
2023-01-04 21:34:57 +0530

    previewView added

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,4 +1,5 @@
 import icons from "url:../../img/icons.svg";
+import previewView from "./previewView";
 import View from "./View";
 
 class BookmarksView extends View {
@@ -7,22 +8,9 @@ class BookmarksView extends View {
   _message = "";
 
   _generateMarkup() {
-    return this._data.map((result) => {
-      const id = window.location.hash.slice(1);
-      return `<li class="preview">
-            <a class="preview__link ${
-              result.id === id ? "preview__link--active" : ""
-            }" href="#${result.id}">
-                <figure class="preview__fig">
-                <img src="${result.imageUrl}" alt="${result.title}" />
-                </figure>
-                <div class="preview__data">
-                    <h4 class="preview__title">${result.title}</h4>
-                    <p class="preview__publisher">${result.publisher}</p>
-                </div>
-            </a>
-        </li>`;
-    });
+    return this._data
+      .map((result) => previewView.render(result, false))
+      .join("");
   }
 }
 
